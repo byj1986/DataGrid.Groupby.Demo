cc3385570714ba1834bd2459e78aca62c173cf82
Bao, Yinjun Bob <byj1986@163.com>
2026-03-16 22:14:47 +0800

    improve details

--- DataGrid.Groupby.Demo/MainWindow.xaml.cs
+++ DataGrid.Groupby.Demo/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 using DataGrid.Groupby.Demo.ViewModels;
+using DevExpress.Xpf.Grid;
 using System.IO;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace DataGrid.Groupby.Demo;
 
@@ -24,5 +27,41 @@ public partial class MainWindow : Window
 
         var jsonPath = Path.Combine(AppContext.BaseDirectory, "cars.json");
         await _viewModel.LoadAsync(jsonPath);
+    }
+
+    private void GridControl_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (IsColumnHeaderElement(e.OriginalSource))
+        {
+            e.Handled = true;
+        }
+    }
+
+    private void GridControl_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        // Disable all right-click interactions inside the grid (menus, etc.).
+        e.Handled = true;
+    }
+
+    private void TableView_ShowGridMenu(object sender, GridMenuEventArgs e)
+    {
+        // DevExpress popup menus (header/row/group) are not WPF ContextMenu; disable at source.
+        e.Handled = true;
+    }
+
+    private static bool IsColumnHeaderElement(object? source)
+    {
+        var current = source as DependencyObject;
+        while (current is not null)
+        {
+            if (current.GetType().Name.Contains("ColumnHeader", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            current = VisualTreeHelper.GetParent(current);
+        }
+
+        return false;
     }
 }
