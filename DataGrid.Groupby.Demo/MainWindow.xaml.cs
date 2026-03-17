a92e4c240181dad23851980553e054b49a59d6a5
Bao, Yinjun Bob <byj1986@163.com>
2026-03-17 22:32:02 +0800

    remove group functions

--- DataGrid.Groupby.Demo/MainWindow.xaml.cs
+++ DataGrid.Groupby.Demo/MainWindow.xaml.cs
@@ -1,11 +1,7 @@
-using DataGrid.Groupby.Demo.Models;
 using DataGrid.Groupby.Demo.ViewModels;
-using System.ComponentModel;
 using System.IO;
 using System.Windows;
-using System.Windows.Data;
 using System.Windows.Input;
-using System.Windows.Threading;
 
 namespace DataGrid.Groupby.Demo;
 
@@ -26,22 +22,6 @@ public partial class MainWindow : Window
 
         var jsonPath = Path.Combine(AppContext.BaseDirectory, "cars.json");
         await _viewModel.LoadAsync(jsonPath);
-
-        // Wait for data binding to apply ItemsSource, then configure grouping and sorting
-        await Dispatcher.InvokeAsync(() =>
-        {
-            var view = CollectionViewSource.GetDefaultView(CarDataGrid.ItemsSource);
-            if (view == null) return;
-
-            using (view.DeferRefresh())
-            {
-                view.GroupDescriptions.Add(new PropertyGroupDescription(nameof(Car.Producer)));
-                view.GroupDescriptions.Add(new PropertyGroupDescription(nameof(Car.CarClass)));
-                view.SortDescriptions.Add(new SortDescription(nameof(Car.Producer), ListSortDirection.Ascending));
-                view.SortDescriptions.Add(new SortDescription(nameof(Car.CarClass), ListSortDirection.Ascending));
-                view.SortDescriptions.Add(new SortDescription(nameof(Car.Model),    ListSortDirection.Ascending));
-            }
-        }, DispatcherPriority.DataBind);
     }
 
     private void GridControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
